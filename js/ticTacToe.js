2448716986bb9f29d15eba7a45429847600e6f6e
Richard Myatt <lydgate77@hotmail.co.uk>
2018-03-31 15:37:52 +0100

    all components in draft.  Board now functional but still in draft.

--- js/ticTacToe.js
+++ js/ticTacToe.js
@@ -10,16 +10,34 @@ Vue.component("screen-three", {
   template: "#screen3"
 });
 
+Vue.component("screen-four", {
+  template: "#screen4"
+});
+
+Vue.component("screen-five", {
+  template: "#screen5",
+  props: ["board"]
+});
+
 
 var main = new Vue({
   el: "#app",
   data: {
     playerone: "Player 1",
     playertwo: "Computer",
-    tokenone: "(X)",
-    tokentwo: "(O)",
-    scoreone: " | 0",
-    scoretwo: " | 0",
-    component: "screen-three"
+    tokenone: "X",
+    tokentwo: "O",
+    scoreone: 0,
+    scoretwo: 0,
+    oneIsActive: true,
+    twoIsActive: false,
+    showPlayers: true,
+    board: ["O", "", "X", "X", "", "X", "", "O", "O"],
+    component: "screen-five"
+  },
+  methods: {
+    playNext: function(i) {
+      this.$set(this.board, i, "X");
+    }
   }
 });
