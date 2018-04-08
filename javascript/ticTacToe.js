3ac0b3a7b5c0aaea6716cdfc52686193ce01c32f
Richard Myatt <lydgate77@hotmail.co.uk>
2018-04-08 18:24:35 +0100

    amended selectBestMove

--- javascript/ticTacToe.js
+++ javascript/ticTacToe.js
@@ -178,7 +178,6 @@ Board.prototype.getAllPosBoards = function() {
 
 // method to rank the move specified by pos which takes an integer 1 - 9.
 Board.prototype.rankMove = function(pos) {
-  debugger;
   var token       = this.getNextTokenToPlay();
   var tempBoard   = this.board.slice();
   if(this.isAvailable(pos)) {
@@ -232,7 +231,7 @@ Board.prototype.selectBestMove = function() {
   // a corner
   for(var k = 0; k < selection.length; k++) {
     if(this.isCorner(selection[k][0])) {
-      return selection[i];
+      return selection[k];
     }
   }
   // if neither a fork or a corner is available to play then return the first
@@ -316,7 +315,6 @@ Board.prototype.isCorner = function(index) {
 // helper function for rankMove to analyse each level given an array of boards
 // and the level
 function analyseLevels(brds, lvl) {
-  debugger;
   // get an array of boards for this level
   var newBoards = [];         // holds all posible boards for this level
   for(var i = 0; i < brds.length; i++) {
@@ -328,7 +326,6 @@ function analyseLevels(brds, lvl) {
   }
   for(var k = 0; k < newBoards.length; k++) {
     var firstBoard = new Board(newBoards[0]);
-    debugger;
     if(newBoards.length === 1 && firstBoard.isWin()) {
       return -10;
     } else if(newBoards.length === 1 && firstBoard.isDraw()) {
@@ -399,6 +396,6 @@ TicTacToe.getFirstLargestSelection = getFirstLargestSelection;
 module.exports = TicTacToe;
 
 
-var brd = new Board(["O", "X", "X", "X", "X", "O", "O", "", ""]);
-var result = brd.selectTopRankedMovesFor();
+var brd = new Board(["X", "", "", "", "O", "", "", "", "X"]);
+var result = brd.selectBestMove();
 console.log(result);
